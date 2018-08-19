b403eeeb09a1b98df9400992f35c0d193c697543
Christian Chavez <christiancchavez10@hotmail.com>
2018-08-18 23:04:49 -0500

    create almost shopping car  interaction and make a changes in html for his better behavior

--- main.js
+++ main.js
@@ -11,9 +11,16 @@ $( document ).ready(function(){
     let divcarousel;
     let countText = 0;
     let carouselTextContainer;
-    let $carouselContainer = $('.js-carousel');
-    let $arrowLeft = $('.js-arrow-left');
-    let $arrowRight = $('.js-arrow-right');
+    const $carouselContainer = $('.js-carousel');
+    const $arrowLeft = $('.js-arrow-left');
+    const $arrowRight = $('.js-arrow-right');
+    const $toggleFilters = $('.js-toggle');
+    const $filterContain = $('.js-filters');
+    const $addShopCarButton = $('.js-btn-add-product');
+    const $shopCarImage = $('.js-shop-car-image');
+    const $amountProducts = $('.js-amount-shop');
+    let counterShop = 0;
+    const $shopCarModal = $('.js-modal-shop');
 
     function moveLeft () {
         if((countImage === 0) && (countText === 0)){
@@ -48,10 +55,6 @@ $( document ).ready(function(){
     setInterval(moveRight, 3000);
     $arrowRight.click(moveRight);
 
-
-    $toggleFilters = $('.js-toggle');
-    $filterContain = $('.js-filters');
-
     function hideShowFilters(){
         if($filterContain.hasClass('lateralSlide')){
             $filterContain.removeClass('lateralSlide')
@@ -59,8 +62,41 @@ $( document ).ready(function(){
             $filterContain.addClass('lateralSlide');
         }  
     };
-
     $toggleFilters.click(hideShowFilters);
 
+
+    const $itemShopCarModal = $('.js-container-shop');
+    $addShopCarButton.click(function(){
+        $shopCarImage.effect( "shake", {times:2, direction:'up'}, 1000 );
+        $amountProducts.text(counterShop += 1);
+
+        const $singleProduct = $(this).parent();
+        let nameProduct = $singleProduct.find('.js-name-product').text();
+        let priceProduct = $singleProduct.find('.js-price-product').text();
+        let imageProduct = $singleProduct.find('.js-image-product').attr('src');
+        let valueSubtotal = $(this).val();
+        let priceProductInteger = parseInt(priceProduct.substring(1,priceProduct.length));
+        let subtotalFull = priceProductInteger * valueSubtotal;
+        $itemShopCarModal.append(`<div class="shopCar__item"><div class="shopCar__item-image"><img class="shopCar__item-image shopCar__item-image--img js-image-shop" src="${imageProduct}" alt=""></div>
+        <div class="shopCar__item-text shopCar__item-text--txt><span class="js-name-shop">${nameProduct}</span></div>
+        <div class="shopCar__item-price shopCar__item-price--tx><span class="js-price-shop">${priceProduct}</span></div>
+        <div class="shopCar__item-amount"><input class="shopCar__item-amount shopCar__item-amount--input js-amount-shop" value="0" type="number" min="1" max="99"></div>
+        <div class="shopCar__item-subtotal">$<span class="shopCar__item-subtotal shopCar__item-subtotal--number js-subtotal-shop"></span></div>
+        <div class="shopCar__item-remove"><img class="shopCar__item-remove js-close-shop" src="./Images/remove.png" alt=""></div></div>`);
+
+        const $amountInputShop = $('.js-amount-shop');
+        let $subtotalInsideShop = $('.js-subtotal-shop');
+        
+        $amountInputShop.click(function(){
+            let valueInsideShop = $(this).val();
+            let newPrice = valueInsideShop * subtotalFull;
+            $(this).next().text(newPrice);
+        });
+    });
+
+
+    $shopCarImage.click(function(){
+        $shopCarModal.removeClass('hide');
+    })
 
 });
