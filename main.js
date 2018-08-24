191766aa22a9c7f4e72bb67f3eb24334ead0688a
Christian Chavez <christiancchavez10@hotmail.com>
2018-08-24 15:15:48 -0500

    modifie the anonimous function and uses in other functions, and modularize the code

--- main.js
+++ main.js
@@ -7,10 +7,7 @@ $( document ).ready(function(){
     const image6 = './Images/surf.jpg';
     const images = [image1, image2, image3, image4, image5, image6];
     const sports = ["skate", "sailing", "skiing", "snowboard", "soccer", "surf"];
-    let countImage = 0;
-    let divcarousel;
-    let countText = 0;
-    let carouselTextContainer;
+    let countCarousel = 0;
     const $carouselContainer = $('.js-carousel');
     const $arrowLeft = $('.js-arrow-left');
     const $arrowRight = $('.js-arrow-right');
@@ -23,55 +20,46 @@ $( document ).ready(function(){
     const $shopCarModal = $('.js-modal-shop');
 
     function moveLeft () {
-        if((countImage === 0) && (countText === 0)){
-            countImage = images.length -1;
-            countImage = countImage;
-            countText = sports.length-1;
-            countText = countText;
+        if((countCarousel === 0)){
+            countCarousel = images.length -1;
         } else {
-            countImage = countImage -1;
-            countText = countText -1;
+            countCarousel = countCarousel -1;
         }
         $carouselContainer.empty();
-        divcarousel = `<img class="carousel__image js-carousel-image" src="${images[countImage]}"/>`;
-        carouselTextContainer = `<span class="carousel__text js-carousel-text ">${sports[countText]}</span>`;
-        $carouselContainer.prepend(divcarousel, carouselTextContainer);
+        appendImagesCarousel();
     };
     $arrowLeft.click(moveLeft);
 
+    function appendImagesCarousel(){
+        const divcarousel = `<img class="carousel__image js-carousel-image" src="${images[countCarousel]}"/>`;
+        const carouselTextContainer = `<span class="carousel__text js-carousel-text ">${sports[countCarousel]}</span>`;
+        $carouselContainer.prepend(divcarousel, carouselTextContainer);
+    }
+
     function moveRight() {
-        if((countImage === images.length-1) && (countImage === images.length-1)){ 
-            countImage = 0;
-            countText = 0;
+        if((countCarousel === images.length-1)){ 
+            countCarousel = 0;
         } else {
-            countImage = countImage +1;
-            countText = countText +1;
+            countCarousel = countCarousel +1;
         }
         $carouselContainer.empty();
-        divcarousel = `<img class="carousel__image js-carousel-image" src="${images[countImage]}"/>`;
-        carouselTextContainer = `<span class="carousel__text js-carousel-text ">${sports[countText]}</span>`;
-        $carouselContainer.prepend(divcarousel, carouselTextContainer);
+        appendImagesCarousel();
     };
-     setInterval(moveRight, 3000);
+    setInterval(moveRight, 3000);
     $arrowRight.click(moveRight);
 
     function hideShowFilters(){
-        if($filterContain.hasClass('lateralSlide')){
-            $filterContain.removeClass('lateralSlide')
-        } else {
-            $filterContain.addClass('lateralSlide');
-        }  
+        $filterContain.toggleClass('lateralSlide')
     };
     $toggleFilters.click(hideShowFilters);
 
-
+    
     const $itemShopCarModal = $('.js-container-shop');
-    let subtotalFull;
     let dataIdClose = 0;
-    let $subtotals;
-    let total;
-    let $containerTotal;
-    $addShopCarButton.click(function(){
+    const $containerTotal = $('.js-total-price-modal');;
+    $addShopCarButton.click(addInfoToModalCart);
+
+    function addInfoToModalCart(){
         $shopCarImage.effect( "shake", {times:2, direction:'up'}, 1000 );
         $amountProducts.text(counterShop += 1);
         const $singleProduct = $(this).parent();
@@ -80,67 +68,73 @@ $( document ).ready(function(){
         let imageProduct = $singleProduct.find('.js-image-product').attr('src');
         let valueSubtotal = $(this).val();
         let priceProductInteger = parseInt(priceProduct.substring(1,priceProduct.length));
-        subtotalFull = priceProductInteger * valueSubtotal;
+        const subtotalFull = priceProductInteger * valueSubtotal;
         $itemShopCarModal.append(`<div class="shopCar__item"><img class="shopCar__item-image js-image-shop" src="${imageProduct}" alt="">
         <span class="js-name-shop">${nameProduct}</span>
         <span class="js-price-shop">${priceProduct}</span>
         <input class="shopCar__item-amount js-amount-shop" value="1" type="number" min="1" max="99">
         $<span class="shopCar__item-subtotal js-subtotal-shop">${subtotalFull}</span>
         <img class="shopCar__item-remove js-cancel-item-shop"data-id="${dataIdClose += 1}" src="./Images/remove.png" alt=""></div>`);
-        $(this).addClass('deactivate');
-        $(this).html('Inside the cart');
-        $(this).attr("disabled", true);
-        $(this).attr('data-id',`${dataIdClose}`);
-       
-        $containerTotal = $('.js-total-price-modal');
-        $subtotals = $('.js-subtotal-shop');
-        total = 0;
+        disableAddToCartButton($(this), dataIdClose)
+        calculateTotalPrice();
+    };
+
+    function disableAddToCartButton(addButton, idClose) {
+        addButton.addClass('deactivate');
+        addButton.html('Inside the cart');
+        addButton.attr("disabled", true);
+        addButton.attr('data-id',`${idClose}`);
+    };
+
+    function calculateTotalPrice() {
+        const $subtotals = $('.js-subtotal-shop');
+        let total = 0;
         $.each($subtotals, function(){
            total += parseInt($(this).text());
-
         });
         $containerTotal.text(total);
+    };
 
-    });
-
-    $itemShopCarModal.on('click', '.js-cancel-item-shop', function(){
+    $itemShopCarModal.on('click', '.js-cancel-item-shop', closeItemModal);
+        
+    function closeItemModal(){ 
         let dataId = $(this).data('id');
         $amountProducts.text(counterShop -= 1);
         const sameBtns = $('.js-single-product').find(`button[data-id ='${dataId}']`);
-        sameBtns.removeClass('deactivate');
-        sameBtns.html('ADD TO CART');
-        sameBtns.attr("disabled", false);
+        removeDeactivate(sameBtns);
         $(this).parent().remove();
-        $subtotals = $('.js-subtotal-shop');
-        total = 0;
-        $.each($subtotals, function(){
-           total += parseInt($(this).text());
-        });
-        $containerTotal.text(total);
-    });
+        calculateTotalPrice();
+    };
+    
+    function removeDeactivate(element){
+        element.removeClass('deactivate');
+        element.html('ADD TO CART');
+        element.attr("disabled", false); 
+    };
 
     let newPrice;
-    $itemShopCarModal.on('click', '.js-amount-shop', function(){
+    $itemShopCarModal.on('click', '.js-amount-shop', newSubtotalItemModal);
+        
+    function newSubtotalItemModal(){
         let valueInsideShop = $(this).val();
         let priceProductPrev = $(this).prev().text();
         let priceProductPrevInt = parseInt(priceProductPrev.substring(1,priceProductPrev.lenght));
         newPrice = valueInsideShop * priceProductPrevInt;
         $(this).next().text(newPrice);
-        $subtotals = $('.js-subtotal-shop');
-        total = 0;
-        $.each($subtotals, function(){
-           total += parseInt($(this).text());
-        });
-        $containerTotal.text(total);
-    });
+        calculateTotalPrice();
+    };
+        
+    
 
     $shopCarImage.click(function(){
         $shopCarModal.removeClass('hide');
-    })
+    });
 
 
     let $btnCLoseShop = $('.js-close-shop');
-    $btnCLoseShop.click(function(){
+    $btnCLoseShop.click(closeModal);
+
+    function closeModal(){
         $shopCarModal.addClass('hide');
         $amountProducts.text(0);
         counterShop = 0;
@@ -148,20 +142,22 @@ $( document ).ready(function(){
         $('.js-total-price-modal').empty();
         const changeAllBtn = $('.js-single-product').find(`button`);
         if(changeAllBtn.hasClass('deactivate')){
-        changeAllBtn.removeClass('deactivate');
-        changeAllBtn.html('ADD TO CART');
-        changeAllBtn.attr("disabled", false);
-        }
+            removeDeactivate(changeAllBtn);
+        };
         $successPurchase.remove();
         $modalBtnBuy.attr("disabled", false);
         $modalBtnBuy.removeClass('getOff');
-    });
+    }
+        
+    
     
     
 
     let $modalBtnBuy = $('.js-btn-buy-modal');
     let $successPurchase;
-    $modalBtnBuy.click(function(){
+    $modalBtnBuy.click(buyPurchase);
+        
+    function buyPurchase(){
         let $containerButtons = $('.js-btns-modal');
         $containerButtons.parent().find('.js-btns-modal').after('<div class="js-success-purchase"><h1>Successful Purchase</h1></div>')
         $successPurchase = $('.js-success-purchase');
@@ -170,7 +166,11 @@ $( document ).ready(function(){
         $successPurchase.html("<h1>Successful Purchase</h1>");
         $(this).attr("disabled", true);
         $(this).addClass('getOff');
-    });
+    };
+        
+    
+
+
 
     let $modalBtnAddItem = $('.js-btn-add-modal');
     $modalBtnAddItem.click(function(){
@@ -182,7 +182,7 @@ $( document ).ready(function(){
 
 
     let $filterOption = $('.js-filter');
-    $filterOption.click(function(){
+    function filteringOptions(){
         let dataFilter = $(this).data("reference");
         let $itemsProducts = $('.js-single-product');
         $itemsProducts.show();
@@ -190,13 +190,14 @@ $( document ).ready(function(){
             $(item).show();
         }else{
             $.each($itemsProducts, function(index, item){
-            const sportItem = $(item).data('sport');
-            if (dataFilter !== sportItem) {
-                $(item).hide();
-            }
+                const sportItem = $(item).data('sport');
+                if (dataFilter !== sportItem) {
+                    $(item).hide();
+                }
             });
         } 
-    });
+    }
+    $filterOption.click(filteringOptions);
 
     
 });
